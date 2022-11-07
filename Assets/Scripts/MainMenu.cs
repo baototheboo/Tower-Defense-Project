dfc06821ac68641ff38cca42842c21305726cb05
Hoangdoan18 <hoangkun1809@gmail.com>
2022-11-07 15:04:26 +0700

    Apply setting and option setting

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,16 +8,49 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public string startScene;
 
+    [SerializeField]
+    public GameObject notify;
+
+    [SerializeField]
+    public GameObject startButton;
+
+    [SerializeField]
+    public GameObject quitButton;
+
+    [SerializeField]
+    public GameObject settingButton;
+
     public void StartGame()
     {
-        AudioManager.instance.PlaySFX(3);
+        AudioManager.instance.PlaySFX(4);
         AudioManager.instance.BossMusic();
         SceneManager.LoadScene(startScene);
     }
 
-    public void OnApplicationQuit()
+    public void OnClickQuitButton()
     {
-        AudioManager.instance.PlaySFX(3);
-        Application.Quit();
+        AudioManager.instance.PlaySFX(4);
+        startButton.SetActive(false);   
+        quitButton.SetActive(false);
+        settingButton.SetActive(false);
+        notify.SetActive(true);        
     }
+
+    public void QuitGame()
+    {
+        AudioManager.instance.PlaySFX(4);
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+
+    public void NotQuitGame()
+    {
+        AudioManager.instance.PlaySFX(4);
+        startButton.SetActive(true);
+        quitButton.SetActive(true);
+        settingButton.SetActive(true);
+        notify.SetActive(false);
+    }
+
+    
 }
